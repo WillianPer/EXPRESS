e68a9652a6f620aae06b5fff0e786489ff359f45
WillianPer <wsresilienttasteshopw@gmail.com>
2022-11-07 21:13:53 -0400

    Tratando a primeira requisição!!

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+// app.use("/opa", (req, res) => {
+//     res.send("Estou <b>bem</b>!!")
+// })
+
+
+app.all("/opa", (req, res) => {
+    res.send("Estou <b>bem</b>!!")
+})
+
+// app.get("/opa", (req, res) => {
+//     res.send("Estou <b>bem</b>!!")
+// })
+
 app.listen(3000, () => {
     console.log("Backend Executando...")
 })
