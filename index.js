e685dabfdcdf2514eb9bf6e1733aad4f3dbb3e6c
WillianPer <wsresilienttasteshopw@gmail.com>
2022-11-08 09:09:04 -0400

    Middleware #01!!

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express')
 const app = express()
 
-// app.use("/opa", (req, res) => {
-//     res.send("Estou <b>bem</b>!!")
-// })
 
+app.use((req, res, next) => {
+    console.log("Antes...")
+    next()
+})
 
-// app.all("/opa", (req, res) => {
-//     res.send("Estou <b>bem</b>!!")
-// })
-
-app.get("/opa", (req, res) => {
-
-     res.json({
+app.get("/opa", (req, res, next) => {
+    console.log("Durante...")
+    
+    res.json({
         data: [
             {id: 7, name: "Ana", position: 1},
             {id: 34, name: "Bia", position: 2},
@@ -22,8 +20,11 @@ app.get("/opa", (req, res) => {
         skip: 0,
         limit: 3,
         status: 200
-     })
-
+    })
+    
+    next()
+    
+    
     // res.json([
     //     {id: 7, name: "Ana", position: 1},
     //     {id: 34, name: "Bia", position: 2},
@@ -39,6 +40,25 @@ app.get("/opa", (req, res) => {
     // res.send("<h1>Estou bem!!</h1><br><br><h2>Tipo é HTML!</h2>")
 })
 
+
+app.use((req, res) => {
+    console.log("Depois...")
+})
+
+
+
 app.listen(3000, () => {
     console.log("Backend Executando...")
 })
+
+
+// app.use("/opa", (req, res) => {
+    //     res.send("Estou <b>bem</b>!!")
+    // })
+    
+    
+    // app.all("/opa", (req, res) => {
+//     res.send("Estou <b>bem</b>!!")
+// })
+
+
