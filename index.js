99cd9148e7b18dfd9eb9e96a92c3a364f90f03e2
WillianPer <wsresilienttasteshopw@gmail.com>
2022-11-08 16:22:09 -0400

    Entendendo o Body-Parser!!

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 const saudacao = require("./saudacaoMid")
+const { json } = require('body-parser')
 
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(saudacao("Wilian"))
 
 
@@ -17,14 +22,17 @@ app.get('/clientes/relatorio', (req, res) => {
 })
 
 app.post('/corpo', (req, res) => {
-    let corpo = ''
-    req.on('data', function(parte) {
-        corpo += parte
-    })
+    // let corpo = ''
+    // req.on('data', function(parte) {
+    //     corpo += parte
+    // })
 
-    req.on('end', function() {
-        res.send(corpo)
-    })
+    // req.on('end', function() {
+    //     res.send(corpo)
+    // })
+    res.send(req.body)
+    // res.send(req.body.nome)
+    // res.send(JSON.stringify(req.body))
 })
 
 app.get('/clientes/:id', (req, res) => {
